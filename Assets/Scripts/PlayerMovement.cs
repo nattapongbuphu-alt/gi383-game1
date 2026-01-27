4d05ef77de8752d5f6dbc136d146a21080833ffa
KhunAA1502 <ying010947@gmail.com>
2026-01-27 15:04:12 +0700

    AnimationPlayer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-
-    Rigidbody2D rb;
-    Vector2 move;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
 
     void Update()
     {
-        move.x = Input.GetAxisRaw("Horizontal");
-        move.y = Input.GetAxisRaw("Vertical");
-        move = move.normalized;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // ������ա������͹����������
+        if (movement.x != 0 || movement.y != 0)
+        {
+            // �ѻവ��ȷҧ੾�е͹������
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+
+            // �觤�� Speed �� 1 ���������蹷���Թ
+            animator.SetFloat("Speed", 1f);
+        }
+        else
+        {
+            // �ҡ��衴������� (Idle) ����觤�� Speed �� 0 �ѹ��
+            animator.SetFloat("Speed", 0f);
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+        // ���������Ф�����͹���
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
     }
 }
